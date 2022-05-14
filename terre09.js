76b43104a538b6d447fd6add549f98db58cf3044
RoMTHoS <romain.matheos.31@gmail.com>
2022-05-14 20:33:53 +0200

    Script qui calcul la puissance d'un nombre

--- terre09.js
+++ terre09.js
@@ -1,13 +1,13 @@
 #! node
 
-const num = process.argv[2]
-const exp = process.argv[3]
-const chi = num - exp
+const num = process.argv[2];
+const exp = process.argv[3];
+const chi = num - exp;
 
 if (isNaN(chi)) {
-  console.log("Veuillez indiquer deux chifres en argument")
+  console.log("Veuillez indiquer deux chiffres en argument");
 } else if (num < 0 || exp < 0) {
-  console.log("Veuillez indiquer un chiffre positif")
+  console.log("Veuillez indiquer un chiffre positif");
 } else {
-  console.log(num ** exp)
+  console.log(num ** exp);
 }
